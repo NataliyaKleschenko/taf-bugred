fd56dc81a4062c298b4c91d68b69e7120a1dac6d
Nataliya Kleschenko <nataliya.kleschenko@gmail.com>
2022-12-18 19:21:16 +0300

    add testng.xml

--- src/test/java/by/itacademy/nataliya/ui/BaseTest.java
+++ src/test/java/by/itacademy/nataliya/ui/BaseTest.java
@@ -2,20 +2,17 @@ package by.itacademy.nataliya.ui;
 
 import by.itacademy.nataliya.ui.driver.WebDriverSingleton;
 import by.itacademy.nataliya.ui.pages.UsersPageLoginForm;
-import org.testng.annotations.AfterClass;
-import org.testng.annotations.AfterTest;
-import org.testng.annotations.BeforeClass;
-import org.testng.annotations.BeforeTest;
+import org.testng.annotations.*;
 
 
 public class BaseTest {
-    @BeforeClass
+    @BeforeMethod
     public void setUp() {
         UsersPageLoginForm page = new UsersPageLoginForm();
         page.openPage();
     }
 
-    @AfterClass
+    @AfterMethod
     public void tearDown() {
         WebDriverSingleton.closeDriver();
     }
